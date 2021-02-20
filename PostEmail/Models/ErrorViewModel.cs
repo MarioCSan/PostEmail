e7ad2abf5704e64a7bbe2173351f5d4e8564cecb
Mario <canalessanchezmario@gmail.com>
2021-02-20 10:15:19 +0100

    Add project files.

---
+++ PostEmail/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PostEmail.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
